258acca00b03b51b35122fb21e16ab55ebcf56b1
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-04-12 09:57:47 +0200

    Include dynamic badges via shields.io.
    * Based on shields.yaml constructed in index.Rmd.
    * Badge code available via modal.
    * Custom tinyurl erum2020-covidr website URL.
    * Like button made badge-looking.

--- contest/_includes/before_gallery.R
+++ contest/_includes/before_gallery.R
@@ -1,4 +1,20 @@
 {{
+
+  `%>%` <- bsplus::`%>%`
+  badge_shield <- sprintf(
+    "https://img.shields.io/badge/dynamic/yaml?label=eRum2020::CovidR&labelColor=2b3990&color=778bc5&query=$.%s&url=https://tinyurl.com/erum2020-covidr/shields.yml",
+    page_name
+  )
+  badge_alt <- "eRum2020::CovidR"
+  page_url <- sprintf(
+    "https://tinyurl.com/erum2020-covidr/%s.html",
+    page_name
+  )
+  badge_md <- sprintf("[![%s](%s)](%s)", badge_alt, badge_shield, page_url)
+  badge_html <- gsub(
+    "</?p>", "",
+    markdown::markdownToHTML(text = badge_md, fragment.only = TRUE)
+  )
   htmltools::tagList(
     htmltools::tags$script(src = "assets/main.js"),
     htmltools::div(
@@ -7,25 +23,44 @@
       htmltools::tags$em("by", author),
       htmltools::tags$sup(
         "[", htmltools::a("repository", href = repository, target = "_blank", .noWS = "outside"), "]"
-      ),
-      htmltools::span(
-        class = "like",
-        htmltools::HTML('
-              <button class="btn btn-primary" data-toggle="collapse" data-target="#utterances">
-                <span class="fa fa-thumbs-up"></span> Like this contribution!
-              </button>
-              <div class="collapse" id="utterances">
-                !!!CHANGE BEFORE MERGING TO MASTER!!!
-                <script src="https://utteranc.es/client.js"
-                        repo="riccardoporreca/test-utterances"
-                        issue-term="pathname" label=":+1:"
-                        theme="github-light"
-                        crossorigin="anonymous" async>
-                </script>
-              </div>
-            ')
-      ),
-      htmltools::hr()
-    )
+      )
+    ),
+    bsplus::bs_modal(
+      id = "badge-modal", title = paste("Include the badge for ", page_name),
+      body = htmltools::tagList(
+        htmltools::p(htmltools::HTML(badge_html)),
+        htmltools::p("Markdown"),
+        htmltools::pre(htmltools::code(badge_md, .noWS = "outside"), .noWS = "outside"),
+        htmltools::p("HTML"),
+        htmltools::pre(htmltools::code(badge_html, .noWS = "outside"), .noWS = "outside")
+      )
+    ),
+    htmltools::div(
+      class = "row badges",
+      htmltools::tag(
+        "button", list(
+          class = "btn btn-badge",
+          htmltools::span(class = "fa fa-thumbs-up"), "Like this contribution!"
+        )
+      ) %>% bsplus::bs_attach_collapse(., "utterances"),
+      htmltools::a(
+        href = "#",
+        htmltools::img(alt = badge_alt, src = badge_shield, .noWS = "outside")
+      ) %>% bsplus::bs_attach_modal(., "badge-modal")
+    ),
+    htmltools::div(
+      class = "collapse", id = "utterances",
+      htmltools::HTML('
+              !!!CHANGE BEFORE MERGING TO MASTER!!!
+              <script src="https://utteranc.es/client.js"
+                repo="riccardoporreca/test-utterances"
+                issue-term="pathname"
+                label=":+1:"
+                theme="github-light"
+                crossorigin="anonymous" async>
+              </script>'),
+    ),
+    htmltools::hr()
   )
+
 }}
