f3e91e36f9f187948d5d6741f77367a4bc98eb48
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-04-12 23:38:38 +0200

    No body padding-bottom only for gallery pages.
    Via JavaScript.

--- contest/assets/main.js
+++ contest/assets/main.js
@@ -1,3 +1,8 @@
+/* We do not want extra space below the galler-after footer */
+$(document).ready(function () {
+  $('body').has('.gallery-container').css({'cssText': 'padding-bottom: 0 !important'});
+});
+
 /* Make the top-level body container full-width and w/o padding */
 $(document).ready(function () {
   $('body>.main-container').css({'max-width': '100%', 'padding': '0'});
