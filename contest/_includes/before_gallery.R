5ff5ef67d8069268d8981d3b45fc66d3bbaada93
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-04-13 11:42:31 +0200

    Streamline usage of `htmltools::` via `tags$`.

--- contest/_includes/before_gallery.R
+++ contest/_includes/before_gallery.R
@@ -1,6 +1,8 @@
 {{
 
+  tags <- htmltools::tags
   `%>%` <- bsplus::`%>%`
+
   badge_shield <- sprintf(
     "https://img.shields.io/endpoint?url=https://runkit.io/riccardoporreca/erum2020-covidr-shield/branches/master/%s",
     page_name
@@ -16,51 +18,52 @@
     markdown::markdownToHTML(text = badge_md, fragment.only = TRUE)
   )
   htmltools::tagList(
-    htmltools::tags$script(src = "assets/main.js"),
-    htmltools::div(
+    tags$script(src = "assets/main.js"),
+    tags$div(
       class = "row",
-      htmltools::h3(title, style = htmltools::css(display = "inline")),
-      htmltools::tags$em("by", author),
-      htmltools::tags$sup(
-        "[", htmltools::a("repository", href = repository, target = "_blank", .noWS = "outside"), "]"
+      tags$h3(title, style = htmltools::css(display = "inline")),
+      tags$em("by", author),
+      tags$sup(
+        "[", tags$a("repository", href = repository, target = "_blank", .noWS = "outside"), "]"
       )
     ),
     bsplus::bs_modal(
       id = "badge-modal", title = paste("Include the badge for ", page_name),
       body = htmltools::tagList(
-        htmltools::p(htmltools::HTML(badge_html)),
-        htmltools::p("Markdown"),
-        htmltools::pre(htmltools::code(badge_md, .noWS = "outside"), .noWS = "outside"),
-        htmltools::p("HTML"),
-        htmltools::pre(htmltools::code(badge_html, .noWS = "outside"), .noWS = "outside")
+        tags$p(htmltools::HTML(badge_html)),
+        tags$p("Markdown"),
+        tags$pre(tags$code(badge_md, .noWS = "outside"), .noWS = "outside"),
+        tags$p("HTML"),
+        tags$pre(tags$code(badge_html, .noWS = "outside"), .noWS = "outside")
       )
     ),
-    htmltools::div(
+    tags$div(
       class = "row badges",
       htmltools::tag(
         "button", list(
           class = "btn btn-badge",
-          htmltools::span(class = "fa fa-thumbs-up"), "Like this contribution!"
+          tags$span(class = "fa fa-thumbs-up"), "Like this contribution!"
         )
       ) %>% bsplus::bs_attach_collapse(., "utterances"),
-      htmltools::a(
+      tags$a(
         href = "#",
-        htmltools::img(alt = badge_alt, src = badge_shield, .noWS = "outside")
+        tags$img(alt = badge_alt, src = badge_shield, .noWS = "outside")
       ) %>% bsplus::bs_attach_modal(., "badge-modal")
     ),
-    htmltools::div(
+    tags$div(
       class = "collapse", id = "utterances",
-      htmltools::HTML('
-              !!!CHANGE BEFORE MERGING TO MASTER!!!
-              <script src="https://utteranc.es/client.js"
-                repo="riccardoporreca/test-utterances"
-                issue-term="pathname"
-                label=":+1:"
-                theme="github-light"
-                crossorigin="anonymous" async>
-              </script>'),
+      htmltools::HTML(
+        # !!!CHANGE BEFORE MERGING TO MASTER!!!
+        '<script src="https://utteranc.es/client.js"
+          repo="riccardoporreca/test-utterances"
+          issue-term="pathname"
+          label=":+1:"
+          theme="github-light"
+          crossorigin="anonymous" async>
+         </script>
+      '),
     ),
-    htmltools::hr()
+    tags$hr()
   )
 
 }}
