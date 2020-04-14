f7d9f7ba0bff4dc890fb45efc212cf55a4e9ae4c
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-04-14 16:37:50 +0200

    Include badges for both shields.io and bagen.net.
    * To assess the stability (shilds.io seems to have issues).
    * Both based on RunKit enpoints from a new user erum2020-covidr

--- contest/_includes/before_gallery.R
+++ contest/_includes/before_gallery.R
@@ -6,20 +6,41 @@
   tags <- htmltools::tags
   `%>%` <- bsplus::`%>%`
 
+  # dymanic badge via shield.io enpoint https://runkit.com/erum2020-covidr/shield
+  # Consider https://tinyurl.com/erum2020-covidr-shield => https://runkit.io/erum2020-covidr/shield/branches/master
   badge_shield <- sprintf(
-    "https://img.shields.io/endpoint?url=https://runkit.io/riccardoporreca/erum2020-covidr-shield/branches/master/%s",
+    "https://img.shields.io/endpoint?url=https://runkit.io/erum2020-covidr/shield/branches/master/%s",
+    page_name
+  )
+  # dymanic badge via badgen.net enpoint https://runkit.com/erum2020-covidr/badge
+  # Consider https://tinyurl.com/erum2020-covidr-badge => https://runkit.io/erum2020-covidr/madge/branches/master
+  badgen <- sprintf(
+    "https://badgen.net/https/runkit.io/erum2020-covidr/badge/branches/master/%s?cache=300",
     page_name
   )
   badge_alt <- "eRum2020::CovidR"
-  page_url <- sprintf(
-    "https://tinyurl.com/erum2020-covidr/%s.html",
-    page_name
-  )
-  badge_md <- sprintf("[![%s](%s)](%s)", badge_alt, badge_shield, page_url)
-  badge_html <- gsub(
-    "</?p>", "",
-    markdown::markdownToHTML(text = badge_md, fragment.only = TRUE)
-  )
+  badge_modal <- function(id, badge) {
+    page_url <- sprintf(
+      "https://milano-r.github.io/erum2020-covidr-contest/%s.html",
+      page_name
+    )
+    badge_md <- sprintf("[![%s](%s)](%s)", badge_alt, badge, page_url)
+    badge_html <- gsub(
+      "</?p>", "",
+      markdown::markdownToHTML(text = badge_md, fragment.only = TRUE)
+    )
+    bsplus::bs_modal(
+      id = id, title = paste("Include the badge for ", page_name),
+      body = htmltools::tagList(
+        tags$p(htmltools::HTML(badge_html)),
+        tags$p("Markdown"),
+        tags$pre(tags$code(badge_md, .noWS = "outside"), .noWS = "outside"),
+        tags$p("HTML"),
+        tags$pre(tags$code(badge_html, .noWS = "outside"), .noWS = "outside")
+      )
+    )
+  }
+
   htmltools::tagList(
     tags$script(src = "assets/main.js"),
     tags$div(
@@ -30,16 +51,8 @@
         "[", tags$a("repository", href = repository, target = "_blank", .noWS = "outside"), "]"
       )
     ),
-    bsplus::bs_modal(
-      id = "badge-modal", title = paste("Include the badge for ", page_name),
-      body = htmltools::tagList(
-        tags$p(htmltools::HTML(badge_html)),
-        tags$p("Markdown"),
-        tags$pre(tags$code(badge_md, .noWS = "outside"), .noWS = "outside"),
-        tags$p("HTML"),
-        tags$pre(tags$code(badge_html, .noWS = "outside"), .noWS = "outside")
-      )
-    ),
+    badge_modal(id = "shield-modal", badge_shield),
+    badge_modal(id = "badge-modal", badgen),
     tags$div(
       class = "row badges",
       htmltools::tag(
@@ -50,8 +63,12 @@
       ) %>% bsplus::bs_attach_collapse(., "utterances"),
       tags$a(
         href = "#",
+        tags$img(alt = badge_alt, src = badgen, .noWS = "outside")
+      ) %>% bsplus::bs_attach_modal(., "badge-modal"),
+      tags$a(
+        href = "#",
         tags$img(alt = badge_alt, src = badge_shield, .noWS = "outside")
-      ) %>% bsplus::bs_attach_modal(., "badge-modal")
+      ) %>% bsplus::bs_attach_modal(., "shield-modal")
     ),
     tags$div(
       class = "collapse", id = "utterances",
